9561016a9b80800244bd46a885585f745042c36c
sarahwonago <sarahshaaleh@gmail.com>
2024-09-25 11:27:36 +0300

    task model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,40 @@
+import uuid
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Task(models.Model):
+    """
+    A model to represent a task.
+
+    Attributes:
+        id(UUID): unique identifier for a task instance.
+
+    """
+
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('done', 'Done'),
+    ]
+
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='pending')
+    priority = models.CharField(max_length=15, choices=PRIORITY_CHOICES, default='medium')
+    due_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
